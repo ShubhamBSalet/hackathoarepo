b42b04dba7abd087f203fcb602ae7ee23deed992
Shubham <129184199+ShubhamBSalet@users.noreply.github.com>
2025-02-15 01:57:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./components/SignUp";
 import Login from "./components/Login";
 import TokenGeneration from "./components/TokenGeneration";
 import CastVote from "./components/CastVote";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Forgot from "./components/Forgot";
+import HomePage from "./components/HomePage";
+import Unauthorized from "./components/Unauthorised";
+
+// ProtectedRoute: Only accessible to logged-in users
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/unauthorized" />;
+};
+
+// PublicRoute: Only accessible to non-logged-in users
+const PublicRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return !token ? children : <Navigate to="/homepage" />;
+};
+
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/token" element={<TokenGeneration />} />
-          <Route path="/vote" element={<CastVote />} />
-          <Route path="/forgot" element={<Forgot />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<PublicRoute><Signup /></PublicRoute>} />
+        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
+        <Route path="/forgot" element={<PublicRoute><Forgot /></PublicRoute>} />
 
-    </>
+        {/* Protected Routes */}
+        <Route path="/homepage" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route path="/token" element={<ProtectedRoute><TokenGeneration /></ProtectedRoute>} />
+        <Route path="/vote" element={<ProtectedRoute><CastVote /></ProtectedRoute>} />
+
+        {/* Unauthorized Route */}
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        {/* Fallback Route */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
