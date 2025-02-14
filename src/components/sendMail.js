48e8de0e6f68e43eaf4c1bb538179634c90ae0cc
Shubham <129184199+ShubhamBSalet@users.noreply.github.com>
2025-02-15 02:54:16 +0530

    Update sendMail.js

--- src/components/sendMail.js
+++ src/components/sendMail.js
@@ -1,33 +1,30 @@
 const nodemailer = require("nodemailer");
 
-const sendMail = async (email, otp) => {
+const sendMail = async (email, voterKey) => {
   try {
     let transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-          user: "thankikeval05@gmail.com",
-          pass: "cfwb nsci olqb fyad",
-        },
-        debug: true, // Enable debug mode
-        logger: true, // Enable logging
-      });
-      
+      service: "Gmail",
+      auth: {
+        user: "thankikeval05@gmail.com",
+        pass: "cfwb nsci olqb fyad",
+      },
+      debug: true, // Enable debug mode
+      logger: true, // Enable logging
+    });
 
     let mailOptions = {
-      from: '"OTP Verification" <your_email@gmail.com>',
+      from: '"Voting System" <your_email@gmail.com>',
       to: email,
-      subject: "Your OTP Code",
-      text: `Your OTP code is: ${otp}`,
+      subject: "Your Voter Key is:",
+      text: `${voterKey}. Use this key to cast your vote.`,
     };
 
     await transporter.sendMail(mailOptions);
-    console.log("OTP sent to email: " + email);
+    console.log("Voter key sent to email: " + email);
   } catch (error) {
     console.error("Error sending email:", error);
   }
 };
-
-// Function to generate a 6-digit OTP
 const generateOtp = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
 };
